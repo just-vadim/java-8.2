841943139e6cfb58e505efa1ec6811e6a0e363e9
Vadim <vadim.kurn@gmail.com>
2020-08-17 12:37:13 +0300

    Code source files updated

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,80 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int minStation;
+    private int maxStation;
+    private int minVolume;
+    private int maxVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < minStation) {
+            return;
+        }
+        if (currentStation > maxStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public void increaseVolume() {
+
+    }
+
+    public void decreaseVolume() {
+
+    }
+
+    public void nextStation() {
+
+    }
+
+    public void previousStation(){
+
+    }
+}
